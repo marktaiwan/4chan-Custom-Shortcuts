5a8b36b9c9b5f48fe563e732886b5ccdde2a1139
Mark Taiwan <marktaiwan@users.noreply.github.com>
2020-01-12 12:41:55 +0800

    Version 1.1.1

--- 4chan-custom-shortcuts.user.js
+++ 4chan-custom-shortcuts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         4chan Custom Shortcuts
 // @description  Configurable shortcuts and enhanced keyboard navigations. "Ctrl+Shift+/" to open settings.
-// @version      1.1.0
+// @version      1.1.1
 // @author       Marker
 // @license      MIT
 // @namespace    https://github.com/marktaiwan/
