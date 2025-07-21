60fe7098fe233a7138c69d078bb8253023f755ad
Mark Taiwan <marktaiwan@users.noreply.github.com>
2025-07-21 16:52:13 +0800

    Restructure eslint config

--- 4chan-custom-shortcuts.user.js
+++ 4chan-custom-shortcuts.user.js
@@ -630,8 +630,8 @@ function isVisible(ele) {
   const {top, bottom, height, mid} = getRect(ele);
   const margin = Math.min(Math.max(50, height / 4), clientHeight / 4);
 
-  return (mid > 0 + margin && mid < clientHeight - margin
-    || top < 0 + margin && bottom > clientHeight - margin);
+  return ((mid > 0 + margin && mid < clientHeight - margin)
+    || (top < 0 + margin && bottom > clientHeight - margin));
 }
 
 function getFirstVisibleOrClosest(selector) {
@@ -1090,8 +1090,8 @@ function openSettings() {
 
   // close panel
   panelWrapper.addEventListener('click', e => {
-    if (e.target == e.currentTarget ||
-      e.target.matches(`#${SCRIPT_ID}--close-button`)) {
+    if (e.target == e.currentTarget
+      || e.target.matches(`#${SCRIPT_ID}--close-button`)) {
       panelWrapper.remove();
     }
   });
